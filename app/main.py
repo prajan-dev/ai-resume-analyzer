9aff081f7828944037a5664902fe29be2d080567
prajan-dev <sujah2026@gmail.com>
2026-05-16 12:59:31 -0400

    allow vercel frontend cors

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ from app.models.resume_model import Resume
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-resume-analyzer-three-virid.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
